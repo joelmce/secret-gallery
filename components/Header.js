d2d3037d64024aa8c74fce35d2f8f943db0151cb
joelmce <joelanthony.mac@gmail.com>
2022-01-31 12:21:20 +1100

    User sessions

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,31 @@
 import header from ".//stylesheets/header.module.css";
 
-const Header = props => (
-<div>
-  <nav className={header.navbar}>
-    <div className={header.navbarinner}>
-      <a href="#colors" className={header.navbarlink}>
-        About
-      </a>
-      <a href="#" className={header.navbarlink}>
-        FAQ
-      </a>
-      <a href="#forms" className={header.navbarlink}>
-        Secret Collective
-      </a>
-    </div>
-    <div className="source_info">
-      <a
-        href="https://webflow.com/website/classNamebook-classNameguide-Template"
-        target="_blank"
-        className={`${header.create_an_experience} ${header.primary_button} ${header.wbutton}`}
-      >
-        Create an Experience
-      </a>
-    </div>
-  </nav>
-  {props.children}
-</div>
-);
+export default function Header(props) {
+  return (
+    <div>
+      <nav className={header.navbar}>
+        <div className={header.navbarinner}>
+          <a href="#colors" className={header.navbarlink}>
+            About
+          </a>
+          <a href="#" className={header.navbarlink}>
+            FAQ
+          </a>
+          <a href="#forms" className={header.navbarlink}>
+            Secret Collective {props.session}
+          </a>
+        </div>
+        <div className="source_info">
+          <a
+            href="/connect"
+            className={`${header.create_an_experience} ${header.primary_button} ${header.wbutton}`}
+          >
+            Create an Experience
+          </a>
+        </div>
+      </nav>
 
-export default Header;
+      {props.children}
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 /** Global */
-import '../styles/globals.css'
+import "../styles/globals.css";
+import styles from "../styles/home.module.css";
 
 /** Ensure styling is normalised globally so we don't get any funky business */
-import 'normalize.css/normalize.css';
+import "normalize.css/normalize.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${styles.container} ${styles.wcontainer}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
