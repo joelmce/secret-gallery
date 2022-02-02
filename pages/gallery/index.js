cc72e4abf8913e10067074ec99a712fee5588653
joelmce <joelanthony.mac@gmail.com>
2022-02-02 14:08:44 +1100

    Better environment

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,5 +1,5 @@
-import React, { Suspense, useEffect, useRef } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import React, { Suspense, useMemo } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   FlyControls,
   Html,
@@ -8,35 +8,34 @@ import {
 } from "@react-three/drei";
 
 import Experiment from ".//Experiment";
-
+import { DirectionalLight } from "three";
 
 function CameraOp() {
   useFrame((state) => {
-    if(state.camera.position.y > 3) {
-      state.camera.position.y = 3
+    if (state.camera.position.y > 3) {
+      state.camera.position.y = 3;
     }
-    if(state.camera.position.y < 3) {
-      state.camera.position.y = 3
+    if (state.camera.position.y < 3) {
+      state.camera.position.y = 3;
     }
-  })
+  });
   return null;
 }
 
 
 export default function Index() {
-
   return (
     <Canvas
-      
       concurrent
       gl={{ alpha: true }}
       camera={{ position: [20, 3, -20], fov: 60 }}
-      shadowMap
+      shadows
       style={{ height: "100vh", width: "100vw", top: 0, left: 0 }}
       colorManagement
-      
+      onCreated={(gl) => {
+        gl.physicallyCorrectLights = true;
+      }}
     >
-      
       <Suspense
         fallback={
           <Html center>
@@ -44,57 +43,13 @@ export default function Index() {
           </Html>
         }
       >
-        <directionalLight
-          intensity={1}
-          position={[0, 100, 0]}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-        />
-        <spotLight
-          position={[0, 7.83, -20.44]}
-          rotation={[0.63, -1.16, 0.01]}
-          intensity={0.2}
-          penumbra={0.15}
-          decay={2}
-          castShadow
-        />
-        <spotLight
-          position={[14.13, 6.11, -21.14]}
-          rotation={[0.63, -1.16, 0.01]}
-          intensity={0.5}
-          penumbra={0.15}
-          decay={2}
-          castShadow
-        />
-        <spotLight
-          position={[22.41, 6.11, -21.14]}
-          rotation={[0.63, -1.16, 0.01]}
-          intensity={0.5}
-          penumbra={1}
-          decay={2}
-          castShadow
-        />
-        <spotLight
-          position={[36.43, 7.83, -20.44]}
-          rotation={[0.63, -1.16, 0.01]}
-          intensity={1}
-          penumbra={0.15}
-          decay={2}
-          castShadow
-        />
-
         <PointerLockControls />
+      
         <Experiment />
 
         <FlyControls movementSpeed={10} />
-        <CameraOp/>
+        <CameraOp />
       </Suspense>
-
     </Canvas>
   );
 }
